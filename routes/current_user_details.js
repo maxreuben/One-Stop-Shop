b3e36cfd634821ab8bba96b6ce7b27e09abb825c
Sri Krishna Kaashyap M <30852667+srikrishnakaashyap@users.noreply.github.com>
2022-11-25 14:23:20 -0500

    change cookie

--- routes/current_user_details.js
+++ routes/current_user_details.js
@@ -3,29 +3,41 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const {get_logged_user_service} = require("../services/get_current_user_details");
+const {
+  get_logged_user_service,
+} = require("../services/get_current_user_details");
 
+app.get(
+  "/get-logged-user",
+  urlencodedParser,
+  async function (request, response) {
+    console.log("Cookies", request.cookies);
 
-app.get("/get-logged-user", urlencodedParser, async function (request, response) {
+    let cookie = request.headers.cookie;
 
-    console.log("Cookies", request.cookies)
+    var output = {};
+    cookie.split(/\s*;\s*/).forEach(function (pair) {
+      pair = pair.split(/\s*=\s*/);
+      var name = decodeURIComponent(pair[0]);
+      var value = decodeURIComponent(pair.splice(1).join("="));
+      output[name] = value;
+    });
 
-    emailId = request.session.emailid;
+    console.log(output);
 
-    console.log("EmailId", emailId)
-    if(emailId == null || emailId == undefined){
-        response.send({"status": 401, "message": "User not logged In"})
+    let emailId = output.emailId;
+
+    console.log("EmailId", emailId);
+    if (emailId == null || emailId == undefined) {
+      response.send({ status: 401, message: "User not logged In" });
+    } else {
+      let res;
+
+      res = await get_logged_user_service(emailId);
+
+      response.send(res);
     }
-    else{
-        let res;
-
-        res = await get_logged_user_service(emailId);
-    
-        response.send(res);
-    }
-
-
-
-});
+  }
+);
 
 module.exports = router;
--- routes/modifyUser.js
+++ routes/modifyUser.js
@@ -7,8 +7,19 @@ const { modifyUserService } = require("../services/modifyUserService");
 
 app.post("/modifyUser", urlencodedParser, function (request, response) {
   data = request.body;
-  // console.log(data.username);
-  data.emailId = request.session.emailid;
+  let cookie = request.headers.cookie;
+
+  var output = {};
+  cookie.split(/\s*;\s*/).forEach(function (pair) {
+    pair = pair.split(/\s*=\s*/);
+    var name = decodeURIComponent(pair[0]);
+    var value = decodeURIComponent(pair.splice(1).join("="));
+    output[name] = value;
+  });
+
+  console.log(output);
+
+  data.emailid = output.emailId;
 
   modifyUserService(data)
     .then(function (responseData) {
