2cb67060b00a6dfb2224bb4bd6c9a2f8d8fa3c75
Tanvi Sayyed <tanvisayyed02@gmail.com>
2022-11-27 20:01:23 -0500

    email services added

---
+++ services/email.js
@@ -0,0 +1,27 @@
+var nodemailer = require('nodemailer');
+async function sendMail(to, subject, text){
+var transport = nodemailer.createTransport({
+    service :'gmail',
+    
+    auth: {
+      user: "process.env.WEBSITE_MAIL",
+      pass: "process.env.WEBSITE_PASSWORD"
+    }
+  });
+
+var mailOptions = {
+  from: 'process.env.WEBSITE_MAIL',
+  to: to,
+  subject: subject,
+  text: text,
+};
+
+transport.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+};
+module.exports = { sendMail };
