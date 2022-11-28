064c72255652f7a38d98312d29046aa576f02a19
Sri Krishna Kaashyap M <30852667+srikrishnakaashyap@users.noreply.github.com>
2022-11-27 19:11:58 -0500

    fix contact us API

--- routes/checkout.js
+++ routes/checkout.js
@@ -3,33 +3,26 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const {checkout} = require("../services/checkout_service");
+const { checkout } = require("../services/checkout_service");
 const cons = require("consolidate");
-var cookieParser = require('cookie-parser')
+var cookieParser = require("cookie-parser");
 
+app.post("/checkout", urlencodedParser, async function (request, response) {
+  let cookie = request.headers.cookie;
 
-app.get("/checkout", urlencodedParser, async function (request, response) {
-
-    let cookie = request.headers.cookie;
-
-
-    var output = {};
-    cookie.split(/\s*;\s*/).forEach(function(pair) {
+  var output = {};
+  cookie.split(/\s*;\s*/).forEach(function (pair) {
     pair = pair.split(/\s*=\s*/);
     var name = decodeURIComponent(pair[0]);
-    var value = decodeURIComponent(pair.splice(1).join('='));
+    var value = decodeURIComponent(pair.splice(1).join("="));
     output[name] = value;
-    });
+  });
 
-    console.log(output);
+  console.log(output);
 
+  let res = await checkout(output.cart, output.emailId);
 
-    let res = await checkout(output.cart, output.emailId);
-
-
-
-    response.send("");
-
+  response.send("");
 });
 
 module.exports = router;
