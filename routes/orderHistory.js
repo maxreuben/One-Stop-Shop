2ed684a5c2d7ef8c376221a1ca169e29d946e6a6
srikargona <39754987+srikargona@users.noreply.github.com>
2022-11-27 19:12:53 -0500

    Added Order History

--- routes/orderHistory.js
+++ routes/orderHistory.js
@@ -1,18 +1,32 @@
-// const { application } = require("express");
 const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const { loginService } = require("../services/loginService");
 const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-// const signupService = require("../services/signupService").signupService;
-const { get_product_details } = require("../services/get_product_details");
+const { get_order_history } = require("../services/get_order_history");
 const url = require('url');
 
 
 app.get("/orderHistory", urlencodedParser, async function (request, response) {
 
-  response.render("orderHistory");
+  let cookie = request.headers.cookie;
+
+    var output = {};
+    cookie.split(/\s*;\s*/).forEach(function (pair) {
+      pair = pair.split(/\s*=\s*/);
+      var name = decodeURIComponent(pair[0]);
+      var value = decodeURIComponent(pair.splice(1).join("="));
+      output[name] = value;
+    });
+
+    let emailId = output.emailId;
+
+    res = await get_order_history(emailId);
+
+    console.log("orders--", res);
+
+  response.render("orderHistory", {orderHistory: res});
   
 });
 
