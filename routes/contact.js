638aa6ecb4e404d783fe84aff140b108d18bc4b3
Sri Krishna Kaashyap M <30852667+srikrishnakaashyap@users.noreply.github.com>
2022-11-29 18:55:54 -0500

    fix contact us

--- routes/contact.js
+++ routes/contact.js
@@ -8,35 +8,27 @@ const {
 
 app.get("/contactus", urlencodedParser, async function (request, response) {
   let cookie = request.headers.cookie;
-  if (cookie == null) {
-    
-
-    response.render("contact",{email_value: ""});
-
-  }
-  else {
+  if (cookie == undefined) {
+    response.render("contact", { email_value: "" });
+  } else {
     var output = {};
-  cookie.split(/\s*;\s*/).forEach(function (pair) {
-    pair = pair.split(/\s*=\s*/);
-    var name = decodeURIComponent(pair[0]);
-    var value = decodeURIComponent(pair.splice(1).join("="));
-    output[name] = value;
-  });
+    cookie.split(/\s*;\s*/).forEach(function (pair) {
+      pair = pair.split(/\s*=\s*/);
+      var name = decodeURIComponent(pair[0]);
+      var value = decodeURIComponent(pair.splice(1).join("="));
+      output[name] = value;
+    });
 
-  console.log(output);
+    console.log(output);
 
-  let emailId = output.emailId;
+    let emailId = output.emailId;
 
-  let res = await get_logged_user_service(emailId);
-  
+    let res = await get_logged_user_service(emailId);
 
-  // userDetails
+    // userDetails
 
-  response.render("contact", { email_value: res['emailId'] });
-
+    response.render("contact", { email_value: res["emailId"] });
   }
-
-  
 });
 
 module.exports = router;
