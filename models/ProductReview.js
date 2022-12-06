63bdafd34d00c381a3ec6927652f6674e9b5405b
komuravellinithishkumar <45819507+komuravellinithishkumar@users.noreply.github.com>
2022-12-06 00:27:04 -0500

    product review

--- models/ProductReview.js
+++ models/ProductReview.js
@@ -2,14 +2,29 @@ const { sequelize } = require("./Connection");
 const Sequelize = require("sequelize");
 
 const ProductReview = sequelize.define("ProductReview",{
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+      },
+    userid:{
+        type: Sequelize.STRING,
+    },
+    productId:{
+        type:Sequelize.INTEGER,
+    },
     productcomments:{
         type : Sequelize.STRING,
     },
     productrating:{
-        type : Sequelize.INTEGER,
+        type : Sequelize.FLOAT,
     },
     productcustomerImage :{
         type :Sequelize.BLOB,
     },
    
 })
+
+ProductReview.sync();
+module.exports = { ProductReview };
