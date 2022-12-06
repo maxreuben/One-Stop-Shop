61b0fce82b8ea2944905b1dbd109442916084e08
Sri Krishna Kaashyap M <30852667+srikrishnakaashyap@users.noreply.github.com>
2022-12-05 19:07:24 -0500

    add order products

--- services/checkout_service.js
+++ services/checkout_service.js
@@ -3,39 +3,46 @@ const Op = Sequelize.Op;
 const { Order } = require("../models/Order");
 const { User } = require("../models/User");
 
+const { OrderProduct } = require("../models/OrderProduct");
+const { Cart } = require("../models/Cart");
 
 async function checkout(cart, emailId) {
+  let user = await User.findOne({
+    where: {
+      emailId: emailId,
+    },
+  });
+  console.log(user);
 
-    let user = await User.findOne({
-        where: {
-          emailId: emailId,
-        },
+  let responseData;
+
+  const order = await Order.create({
+    UserId: user.id,
+    orderAmount: 100,
+  })
+    .then(function (item) {
+      console.log("ITEM", item);
+      responseData = {
+        message: "Order Created",
+        status: 200,
+        error: "",
+        userObject: item,
+      };
+
+      let cart = Cart.findOne({ where: { UserId: user.id } }).then(function (
+        item
+      ) {
+        let userCart = item.cart;
+        console.log("USER CART", userCart);
+      });
+    })
+    .catch(function (error) {
+      console.log("ERROR", error);
+      console.log(user);
+      responseData = { message: "Error", status: 501, error: error.errors };
     });
-    console.log(user);
 
-    let responseData;
-
-    const order = await Order.create({
-        UserId:user.id,
-        orderAmount:100
-      })
-        .then(function (item) {
-          console.log("ITEM", item);
-          responseData = {
-            message: "Order Created",
-            status: 200,
-            error: "",
-            userObject: item,
-          };
-        })
-        .catch(function (error) {
-          console.log("ERROR", error);
-          console.log(user);
-          responseData = { message: "Error", status: 501, error: error.errors };
-        });
-
-        return responseData;
-
-} 
+  return responseData;
+}
 
 module.exports = { checkout };
